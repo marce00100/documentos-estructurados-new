3930034e6e9528e5e6740acb658253de8e52eb7c
Javier Marcelo Fernandez Guerrero <mfernandez@geo.gob.bo>
2015-12-15 00:38:24 -0400

    #############

--- destruct-b/routes/rutas.js
+++ destruct-b/routes/rutas.js
@@ -1,17 +1,14 @@
     var express = require('express');
     var router = express.Router();
-    /* GET home page. */
-    router.get('/', function(req, res, next) {
-        res.render('index', {title: 'Titulo FFFFFF'});
-    });
-    module.exports = router;
-    var dominio = "/v2",
-        usuario_crea = "100",
-        usuario_modifica = "201";
+
     var mongoose = require('mongoose');
+    var Plantilla = mongoose.model('Plantilla');
+    var Documento = mongoose.model('Documento');
+    var dominio = "/v2",
+    usuario_crea = "100",
+    usuario_modifica = "201";
     /****************************  PLANTILLAS **********************************************/
 
-    var Plantilla = mongoose.model('Plantilla');
     router.get(dominio + '/plantillas', function(req, res, next)
     {
         Plantilla.find(function(err, plantillasRes, count)
@@ -22,6 +19,7 @@
             }, 200);
         });
     });
+
     router.get(dominio + '/plantillas/:id', function(req, res, next)
     {
         Plantilla.findById(req.params.id, function(err, plantillaRes)
@@ -32,12 +30,24 @@
             }, 200);
         });
     });
+
+    router.get(dominio + '/plantillasvigentes', function(req, res, next)
+    {
+        plantillaRes =Plantilla.where("vigente", "=", true);//.orderBy('nombre', 'ASC').get();
+        console.log(plantillaRes);  
+//        res.json({
+//                mensaje: "Encontradax",
+//                plantilla: plantillaRes
+//            }, 200);
+
+    });
+
     router.post(dominio + '/plantillas', function(req, res, next)
     {
         var comp = req.body.componentes;
         var componentesJson = typeof (comp) === "string" ? JSON.parse(comp) : comp;
         plantilla = new Plantilla();
-        plantilla.nombre = (typeof req.body.nombre === "undefined") ? "" : req.body.nombre; 
+        plantilla.nombre = (typeof req.body.nombre === "undefined") ? "" : req.body.nombre;
         plantilla.descripcion = (typeof req.body.descripcion === "undefined") ? "" : req.body.descripcion;
         plantilla.vigente = parseInt(req.body.vigente);
         plantilla.componentes = componentesJson;
@@ -59,7 +69,7 @@
         var componentesJson = typeof (comp) === "string" ? JSON.parse(comp) : comp;
         Plantilla.findById(req.params.id, function(err, plantilla)
         {
-            plantilla.nombre = (typeof req.body.nombre === "undefined") ? "" : req.body.nombre; 
+            plantilla.nombre = (typeof req.body.nombre === "undefined") ? "" : req.body.nombre;
             plantilla.descripcion = (typeof req.body.descripcion === "undefined") ? "" : req.body.descripcion;
             plantilla.vigente = parseInt(req.body.vigente);
             plantilla.componentes = componentesJson;
@@ -76,7 +86,6 @@
     });
     /****************************  DOCUMENTOS **********************************************/
 
-    var Documento = mongoose.model('Documento');
     router.get(dominio + '/documentos', function(req, res, next)
     {
         Documento.find(function(err, docRes, count)
@@ -137,60 +146,8 @@
             });
         });
     });
-//    /********************* ejemplo  ******************* */
-//
-//
-//    var Curso = mongoose.model('Curso');
-//
-////http://localhost:3000/create
-//    router.post('/create', function(req, res, next)
-//    {
-//        new Curso({
-//            nombre: req.body.nombre,
-//            contenido: req.body.contenido,
-//            updated_at: Date.now()
-//        }).save(function(err, curso, count)
-//        {
-//            res.send(curso);
-//        });
-//    });
-//
-////http://localhost:3000/all
-//    router.get('/all', function(req, res, next)
-//    {
-//        Curso.find(function(err, cursos, count)
-//        {
-//            res.send(cursos);
-//        });
-//    });
-//
-////http://localhost:3000/curso/Phalcon
-//    router.get('/curso/:name', function(req, res, next)
-//    {
-//        Curso.findOne({nombre: req.params.name}, function(err, curso)
-//        {
-//            res.send(curso);
-//        });
-//    });
-//
-////http://localhost:3000/update/544cdf1b412c1ba537848002
-//    router.put('/update/:id', function(req, res, next)
-//    {
-//        Curso.findOneAndUpdate({_id: req.params.id}, {nombre: req.body.nombre, contenido: req.body.contenido})
-//            .exec(function(err, curso, count)
-//            {
-//                res.send(curso);
-//            });
-//    });
-//
-//
-////http://localhost:3000/delete/544ce99d4b29efcf3a7e26d5
-//    router.delete('/delete/:id', function(req, res, next)
-//    {
-//        Curso.findOneAndRemove({_id: req.params.id})
-//            .exec(function(err, curso, count)
-//            {
-//                res.send({res: "Success"});
-//            });
-//    });
-
+    /*++++++++++++++++++++++++++++++++++++++*/
+    router.get('/', function(req, res, next) {
+        res.render('index', {title: 'Titulo FFFFFF'});
+    });
+    module.exports = router;
